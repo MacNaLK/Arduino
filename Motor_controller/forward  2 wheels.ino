64bec59e7f4c2b8a63e47a39d4fd78a39b92fd64
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-10-30 12:54:48 +0530

    Implement serial control for motor movements with commands for forward, backward, left, right, and stop

--- Motor_controller/forward  2 wheels.ino
+++ Motor_controller/forward  2 wheels.ino
@@ -2,12 +2,31 @@
 
 L298N m1 = L298N(9,8,7); //  ENA , IN2 , IN1
 L298N m2 = L298N(11,12,13); //  ENB , IN4 , IN3
+
 void setup() {
-m1.setSpeed(255);
-m2.setSpeed(255);
+  Serial.begin(9600);
+  m1.setSpeed(255);
+  m2.setSpeed(255);
 }
 
 void loop() {
-  m1.forward();
-  m2.forward();
+  if (Serial.available()) {
+    char command = Serial.read();
+    if (command == 'F') {
+      m1.forward();
+      m2.forward();
+    } else if (command == 'B') {
+      m1.backward();
+      m2.backward();
+    } else if (command == 'L') {
+      m1.backward();
+      m2.forward();
+    } else if (command == 'R') {
+      m1.forward();
+      m2.backward();
+    } else if (command == 'S') {
+      m1.stop();
+      m2.stop();
+    }
+  }
 }
